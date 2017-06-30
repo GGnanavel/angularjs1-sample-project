bc2c9bce3a59262580f379441a58238be6d879ce
GGnanavel <gnanavel@aryvart.com>
2017-06-30 15:56:44 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,20 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '123';
+$dbname = 'whiteboard';
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
+
+mysql_select_db($dbname);
+
+
+    $row = mysql_query("select * from chat");
+
+	$rows = mysql_fetch_array($row);
+
+	echo "<pre>";
+	print_r($rows);
+	exit;
+
+?>
