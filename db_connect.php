b20a1d1fc98258a05778fd6f474175ec1d1db11f
GGnanavel <gnanavel@aryvart.com>
2017-12-09 16:29:26 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = 'localhost';
 $dbuser = 'root';
-$dbpass = '01111';
+$dbpass = '001111';
 $dbname = 'whiteboard';
 
 $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
